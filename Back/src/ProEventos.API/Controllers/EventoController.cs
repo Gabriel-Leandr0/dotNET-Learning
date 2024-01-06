7f08186c58e0f9fcc8f61a5dc8b1e32686f1a28e
Gabriel <gabriel.leandrods@gmail.com>
2024-01-05 21:49:30 -0300

    Criando primeiros controllers

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public EventoController()
+        {
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Exemplo GET";
+        }
+        
+        [HttpPost]
+        public string Post()
+        {
+            return "Exemplo POST";
+        }
+        
+        [HttpPut("{id}")]
+        public string Put()
+        {
+            return "Exemplo PUT";
+        }
+
+        [HttpDelete("{id}")]
+        public string Delete()
+        {
+            return "Exemplo DELETE";
+        }
+    }
+}
