c4cd5e0651207eb8232bb1aeb2b3ea02e80b038b
Gabriel Leandro de Santana <gabriel.leandro@modalgr.com.br>
2024-01-09 15:44:21 -0300

    Bootstrap 5 adicionado ao projeto

--- Front/ProEventos-APP/src/app/app.module.ts
+++ Front/ProEventos-APP/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { EventosComponent } from './components/eventos/eventos.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { EventosComponent } from './components/eventos/eventos.component';
     EventosComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
